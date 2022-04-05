721bf53dd0c434b8e99f54b8f90c5e79b099c45c
sammitako <p.hansaem@hotmail.com>
2022-04-05 20:49:27 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import Counter from "./Counter";
 import FunctionalGreetings from "./FunctionalGreetings";
 import Greetings from "./Greetings";
+import MyForm from "./MyForm";
 
 function App() {
   const onClick = (name: string) => {
     console.log(name);
   };
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <>
       <Greetings name="SAM" />
       <FunctionalGreetings name="SEAN" onClick={onClick} />
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 }
